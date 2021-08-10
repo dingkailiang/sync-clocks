e238040e731165108dcc3ce97d43b3042dc8369c
Dingkai Liang <dingkailiang@gmail.com>
2021-08-09 22:32:35 -0600

    create basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+    <el-container>
+        <el-header>
+            <h1>Synchronized Clocks</h1>
+        </el-header>
+        <el-main>
+            <el-form>
+                <el-form-item label="Time">
+                    <el-time-picker
+                        v-model="time"
+                    ></el-time-picker>
+                </el-form-item>
+            </el-form>
+                <el-row :gutter="30">
+                <el-col :span="12">
+                    <el-card header="Digital Clock"></el-card>
+                </el-col>
+                <el-col :span="12">
+                    <el-card header="Analog Clock"></el-card>
+                </el-col>
+            </el-row>
+        </el-main>
+    </el-container>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "@vue/reactivity"
+import {ElContainer,ElHeader,ElMain,ElCard,ElRow,ElCol,ElTimePicker,ElFormItem,ElForm} from "element-plus"
+// Init time as current time
+const time = ref(new Date())
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+// Increase time every second
+setInterval(()=>{
+    time.value = new Date(time.value.getTime() + 1000)
+},1000)
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+    background-color:whitesmoke;
+}
+.el-header {
+    border-bottom: 1px solid #dcdfe6
 }
 </style>
